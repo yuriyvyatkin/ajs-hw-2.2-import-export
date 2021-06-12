92a889a819d1250a2f10d2d0f216ed59cde9fe48
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2021-06-12 16:19:09 +1000

    Complete HW 2.2

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import Game, {
+  GameSavingData,
+  readGameSaving as loadGame,
+  writeGameSaving as saveGame
+} from './game';
+
+console.log('app worked');
+
+const game = new Game();
+game.start();
